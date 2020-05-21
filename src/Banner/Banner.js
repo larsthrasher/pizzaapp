7b9b9ac67cc85433fdf61ce0f87bffe787bb9697
larsthrasher <thrasherlars@gmail.com>
2020-05-21 17:30:12 -0400

    foodGrid

--- src/Banner/Banner.js
+++ src/Banner/Banner.js
@@ -1,8 +1,9 @@
-import styled from 'styled-components';
+import styled from "styled-components";
 
 export const Banner = styled.div`
   height: 200px;
-  background-image: url('img/banner.jpg');
-  background-positon: center;
+  background-image: url(img/banner.jpg);
+  background-position: center;
   background-size: cover;
-`
+  filter: contrast(75%);
+`;
